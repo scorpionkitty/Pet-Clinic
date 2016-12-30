74fe47686729dbae0a348c6d3ccb865ab2b67541
David Sibrian <davidsibrian08@gmail.com>
2016-12-30 14:32:25 -0500

    Added Selenium tests for the Pet Clinic website (in-progress)
    Website final mockup in progress

---
+++ src/test/java/com/revature/selenium/SeleniumDriver.java
@@ -0,0 +1,13 @@
+package com.revature.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDriver {
+	
+	public WebDriver makeDriver(String driverPath) {
+		System.setProperty("webdriver.chrome.driver", driverPath);
+		WebDriver driver = new ChromeDriver();
+		return driver;
+	}
+}
