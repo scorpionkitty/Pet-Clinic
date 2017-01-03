7db588648b965030c294a2ec85f5c12f40fe362e
David Sibrian <davidsibrian08@gmail.com>
2017-01-03 17:00:21 -0500

    Created a SessionFactory as a singleton. Started Facade

---
+++ src/main/java/com/revature/data/VetDAO.java
@@ -0,0 +1,17 @@
+package com.revature.data;
+
+import java.util.List;
+import org.hibernate.Session;
+import beans.User;
+
+public class VetDAO {
+	private Session session;
+	
+	public void setSession(Session session) {
+		this.session = session;
+	}
+
+	public List<User> getAll() {
+		return session.createCriteria(User.class).list();
+	}
+}
