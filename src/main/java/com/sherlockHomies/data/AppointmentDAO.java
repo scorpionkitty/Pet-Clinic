5e90f7f25ce566344901458c96a8b13f7192d02c
shamlau <shamlau23@gmail.com>
2017-01-04 19:29:00 -0500

    Created new AppointmentDAO

---
+++ src/main/java/com/sherlockHomies/data/AppointmentDAO.java
@@ -0,0 +1,39 @@
+package com.sherlockHomies.data;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
+
+import com.sherlockHomies.beans.Appointment;
+
+/**
+ * DAO that performs crud operations on appointments
+ * will add more methods as we go on
+ * @author Sam
+ *
+ */
+@Repository(value="appointmentdao")
+@Scope(value="prototype")
+public class AppointmentDAO {
+	
+	private Session session;
+
+	public Appointment getAppointment(int id){
+		return (Appointment) session.get(Appointment.class, id);
+	}
+	
+	public void setSession(Session session){
+		this.session = session;
+	}
+	
+	public List<Appointment> getAll(){
+		return session.createCriteria(Appointment.class).list();
+	}
+	
+	public void insert(Appointment appointment){
+		session.save(appointment);
+	}
+
+}
