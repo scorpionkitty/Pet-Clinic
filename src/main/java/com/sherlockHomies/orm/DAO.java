e43778da3b017dee43e2d425fd905d35917a3296
Karina Macancela <karymacancela@gmail.com>
2017-01-07 12:21:07 -0500

    first successful publish using tomcat

--- src/main/java/com/sherlockHomies/orm/DAO.java
+++ src/main/java/com/sherlockHomies/orm/DAO.java
@@ -1,6 +1,9 @@
 package com.sherlockHomies.orm;
 
 import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,6 +14,9 @@ import org.springframework.transaction.annotation.Transactional;
  *
  */
 
+@Repository(value="dao")
+@Scope(value="prototype") 
+//@Qualifier("sessionFactory")
 public class DAO {
 
 	private SessionFactory sessionFactory;
--- src/main/java/com/sherlockHomies/web/BusinessDelegate.java
+++ src/main/java/com/sherlockHomies/web/BusinessDelegate.java
@@ -1,11 +1,20 @@
 package com.sherlockHomies.web;
 
 import java.util.List;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import com.sherlockHomies.beans.User;
+import com.sherlockHomies.orm.Facade;
 
-public class Delegate {
+@Service(value="businessDelegate")
+public class BusinessDelegate {
 
+	//@Autowired
+	private Facade facade;
+	public void setFacade(Facade facade) {
+		this.facade = facade;
+	}
+	
 	public User getUserById(int id) {
 		// TODO Auto-generated method stub
 		return null;
